200bbeea23ab05ceacd1f9794b11e6233f5e66e7
Noor-abid <abidnoor38544@gmail.com>
2025-08-26 21:43:46 +0500

    initial

---
+++ WorkingWithJSONFiles.py
@@ -0,0 +1,9 @@
+import json
+college={
+"college": "Engineering college",
+"objectives": "Mastering Electrical and Computer Engineering";
+"departments":{
+    "dept1": "Electrical",
+    dept2
+}
+}
